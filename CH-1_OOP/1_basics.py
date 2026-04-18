6b4de36a0dcc23907f15df7a2f48c1dbe2ec9be6
AdnanISB <developernew041@gmail.com>
2026-04-18 22:46:38 +0500

    First commit

---
+++ CH-1_OOP/1_basics.py
@@ -0,0 +1,15 @@
+class MyClass:
+
+    var1 = "red"
+    var2 = "apple"
+
+    def func1(self):
+        print("Hello World")
+
+    def func2(self):
+        print("Hello Globe")
+    
+obj = MyClass()
+obj1 = MyClass()
+obj.func1()
+obj1.func2()
